cbbf11e8e9a18103ef8456210ef1e4bb9b5b284d
usmanmobeen111 <thecodeweaver111@gmail.com>
2026-05-14 20:31:46 +0500

    Initialize express server with mongoose database connection and typescript configuration

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
-
+import connectDB from "./config/db.ts"
 dotenv.config()
+connectDB()
 const PORT = process.env.PORT || 4000
 const app = express()
 
