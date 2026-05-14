06f1156f7b9c4253385a0eed6dcf65ccfaa27fa5
usmanmobeen111 <thecodeweaver111@gmail.com>
2026-05-14 14:50:38 +0500

    Initial Commit

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+const PORT = process.env.PORT || 4000
+const app = express()
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res)=>{
+    res.send("Haaaaaalo")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`)
+})
